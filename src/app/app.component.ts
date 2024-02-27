13e97a491e2f2c33c72d0e385b4a46d2a274a229
Vlaimir Korba <krissky1986@gmail.com>
2024-02-27 16:50:40 +0200

    #107 - What are Pipes in Angular | Angular Pipes |

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AdminComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-pipes';
